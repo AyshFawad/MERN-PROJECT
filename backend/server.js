76ced8823f1b4c21ee178ba5a0cbb759b3487a27
Ayesha Banu <ayesha.fawad12@gmail.com>
2025-04-14 21:13:59 -0400

    product model created and post  request function code written

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './model/product.model.js';
 
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/products",(req,res)=>{
-    res.send("server is ready ");
+app.post("/api/products",async (req,res)=>{
+    const product =req.body;
+    if(!product.name|| !product.price ||!product.image){
+        return res.status(400).json({ success:false, message:"Please provide all fields"});
+    }
+
+    const newProduct = new Product(product)
+    try {
+        await newProduct.save();
+        res.status(201).json({success: true, data: newProduct})
+    } catch (error) {
+        console.error ("Error in Creating the product", error.message);
+        res.status(500).json({success: false, message: "Server Error"});
+    }
 })
 
-console.log(process.env.MONGO_URI)
 app.listen(5000, ()=> {
     connectDB();
     console.log("Server running");
