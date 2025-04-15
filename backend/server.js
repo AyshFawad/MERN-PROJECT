7593ece0103a718419e6191840bf56291af05e83
Ayesha Banu <ayesha.fawad12@gmail.com>
2025-04-15 12:30:30 -0400

    get post delete update endpoints coded

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './model/product.model.js';
+import mongoose from 'mongoose';
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.get("/api/products", async (req,res)=> {
+    try {
+        const products = await Product.find({})
+        res.status(201).json({success: true, data: products})
+    }catch (error) {
+        console.log("error in fetching products:",error.message);
+        res.status(500).json({success:false,message:"Server Error"})
+    }
+})
 
 app.post("/api/products",async (req,res)=>{
     const product =req.body;
@@ -25,6 +36,32 @@ app.post("/api/products",async (req,res)=>{
     }
 })
 
+app.put("/api/products/:id", async (req,res)=> {
+    const {id} =req.params;
+    const product =req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid Product Id"})
+    }
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id,product,{new:true})
+        res.status(201).json({success: true, data: updatedProduct})
+    }catch (error) {
+        console.log("error in updated products:",error.message);
+        res.status(500).json({success:false,message:"Server Error"})
+    }
+})
+
+app.delete ("/api/products/:id", async (req,res)=> {
+    const {id} = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success :true, message:"Product deleted"});
+        } catch (error) {
+            res.status(404).json({success : false ,message:"Product not found"});
+        
+    }
+})
 app.listen(5000, ()=> {
     connectDB();
     console.log("Server running");
