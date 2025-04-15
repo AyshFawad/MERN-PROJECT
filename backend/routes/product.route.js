f579082a8217dc7fd549232884cff5ed0ba3c224
Ayesha Banu <ayesha.fawad12@gmail.com>
2025-04-15 12:59:31 -0400

    routes folder and controllers created

---
+++ backend/routes/product.route.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { createProducts, deleteProduct, getProducts, updateProduct } from '../controller/products.controller.js';
+const router = express.Router();
+
+router.get("/",getProducts );
+
+router.post("/",createProducts);
+
+router.put("/id", updateProduct);
+
+router.delete ("/:id", deleteProduct );
+
+export default router;
